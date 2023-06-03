55d1380fda01759d1e45cec12850189853261cdc
Tom Barto <tombarto@Toms-MacBook-Air.local>
2023-06-03 10:43:30 -0400

    added read todo list to app

--- react-to-do/src/App.js
+++ react-to-do/src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
+import TodoList from './components/TodoList';
 
 function App() {
+    const [todos, setTodos] = useState([
+        { complete: false, task: "Read about MongoDb" },
+        { complete: false, task: "Create a React ToDo App" },
+        { complete: false, task: "Find my key" }
+      ]);
   return (
     <div className="App">
-		
+      <TodoList todos={todos}></TodoList>
     </div>
   );
 }
