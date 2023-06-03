8d5a1e2e7a59f5e35b2f9d0ed3e2fb718664a316
Tom Barto <tombarto@Toms-MacBook-Air.local>
2023-06-03 10:55:58 -0400

    added mark complete

--- react-to-do/src/App.js
+++ react-to-do/src/App.js
@@ -1,19 +1,24 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import TodoList from './components/TodoList';
+import TodoForm from './components/TodoForm';
 
 function App() {
-    const [todos, setTodos] = useState([
-        { complete: false, task: "Read about MongoDb" },
-        { complete: false, task: "Create a React ToDo App" },
-        { complete: false, task: "Find my key" }
-      ]);
-  return (
-    <div className="App">
-      <TodoList todos={todos}></TodoList>
-    </div>
-  );
+	const [todos, setTodos] = useState([
+		{ complete: false, task: "Read about MongoDb" },
+		{ complete: false, task: "Create a React ToDo App" },
+		{ complete: false, task: "Find my key" }
+	]);
+	return (
+		<div className="App">
+			<TodoForm addTodo={(todo) => {
+				if (todo.task.trim().length > 0) {
+					setTodos([...todos, todo]);
+				}
+			}} />
+			<TodoList todos={todos} updateTodos={(list) => { setTodos(list) }}></TodoList>
+		</div>
+	);
 }
 
 export default App;
--- react-to-do/src/components/TodoList.js
+++ react-to-do/src/components/TodoList.js
@@ -2,17 +2,26 @@
 import React from 'react';
 
 function TodoList({ todos, updateTodos }) {
-  return (
-    <div className="todo-list">
-      {todos.map((todo, index) => (
-        <div
-          key={index}
-          className="todo">
-          Item {index + 1}: {todo.task}
+
+    const markComplete = (id) => {
+        const updatedList = todos.map((item, index) => {
+            if (id !== index) return item;
+            return { ...item, complete: !(item.complete) };
+        });
+        updateTodos(updatedList);
+    }
+    return (
+        <div className="todo-list">
+            {todos.map((todo, index) => (
+                <div
+                    key={index}
+                    className={`todo ${todo.complete ? "taskDone" : ""}`}
+                    onClick={() => markComplete(index)}>
+                    Item {index + 1}: {todo.task}
+                </div>
+            ))}
         </div>
-      ))}
-    </div>
-  );
+    );
 };
 
 export default TodoList;
